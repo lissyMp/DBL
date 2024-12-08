d1d236d3112063d976b1f7cbd3c3bce88fa4d593
lissyMp <ymachacca@unsa.edu.pe>
2024-12-08 15:45:10 -0500

    hasta el momento muestra los procedimientos almacenados cuando colocas su url por ejemplo :http://127.0.0.1:5000/procedimiento1/18 , ahora vamos a trabajar para generar botones conel cual evitaremos poner todo la url , ya que al darle click y llenar unos parametros nos mostrara automaticamente la tabla con los resultados.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask , render_template
 
 import mysql.connector
 
-connection = mysql.connector.connect(host = 'localhost',port = '3306', username ='root', database = 'CINE' , password = '111')
+connection = mysql.connector.connect(host = 'localhost',port = '3306', username ='root', database = 'CIN3' , password = '111')
 
 cursor = connection.cursor()
 
@@ -30,6 +30,7 @@ def Persona():
 def regi():
   return "Acontinuacion detalles de la pelicula (puedes borrar esta funcion )"
 
+######################################################################
 @app.route("/peliculita")
 def Pelicula():
   cursor.execute("select * from Pelicula ")
@@ -37,25 +38,39 @@ def Pelicula():
   return render_template("pelicula.html" , data = value , name= 'PELICULA')
   
   
+######################################################################
 @app.route("/procedimiento1/<int:codigo_actor>")
 def Procedimiento1(codigo_actor):
     try:
-        # Llamar al procedimiento almacenado
+    
         cursor.callproc('ObtenerPeliculasPorActor', [codigo_actor])
 
-        # Recuperar resultados
         results = []
         for result in cursor.stored_results():
             results = result.fetchall()
 
-        # Renderizar los datos en una plantilla
         return render_template("procedimiento1.html", data=results, actor_id=codigo_actor)
 
     except mysql.connector.Error as e:
         return f"Error al ejecutar el procedimiento almacenado: {e}"
 
-    
-  
+###################################################################
+
+@app.route("/procedimiento2/<fecha_inicio>/<fecha_fin>" )
+def Procedimiento2(fecha_inicio , fecha_fin):
+    try:
+        cursor.callproc('ObtenerEmpleadosPorFechaNacimiento',[fecha_inicio , fecha_fin])
+        results = []
+        
+        for result in cursor.stored_results():
+            results = result.fetchall()
+        
+        return render_template("procedimiento2.html", data = results ,fecha_inicio = fecha_inicio , fecha_fin = fecha_fin)
+    except mysql.connector.Error as e:
+        return f"Error al ejecutar el procedimiento almacenado: {e}"
+
+
+###################################################################  
 if __name__ == "__main__":
   app.run(debug=True)
 
